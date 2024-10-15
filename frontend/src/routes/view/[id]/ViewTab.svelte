ac913efa4b4ee20cf55e0f1051ee3e8267fd474b
Leonard Forsberg <leonard.forsberg@vx.se>
2024-10-15 23:22:25 +0200

    allow mouse inputs to form view but warn about it

--- frontend/src/routes/view/[id]/ViewTab.svelte
+++ frontend/src/routes/view/[id]/ViewTab.svelte
@@ -4,10 +4,15 @@
 	export let id;
 </script>
 
-<p class="text-center">This is how the form will look like to the users.</p>
+<p class="mt-4 text-center text-lg font-semibold dark:text-gray-200">
+	This is how the form will look like to the users.
+</p>
+<p class="mt-2 text-center font-bold text-red-600">
+	WARNING: This is a live view of the form. Any submissions will be recorded.
+</p>
 
 <iframe
 	src={(PUBLIC_PUBLIC_URL ?? 'http://localhost:8889') + '/form/' + id}
 	title="Form View"
-	class="pointer-events-none h-screen w-full"
+	class="h-screen w-full"
 ></iframe>
